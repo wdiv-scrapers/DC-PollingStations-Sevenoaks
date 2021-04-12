c82779397de7f1752c9011c98f5159cee74199c6
GeoWill <roper.william@gmail.com>
2021-04-12 12:22:57 +0100

    Update council id

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 stations_url = "https://opendata.arcgis.com/datasets/5adf5af49995429b9527ef64d94c7be6_0.geojson"
 districts_url = "https://opendata.arcgis.com/datasets/105585d2718f480d9703973d134aaa40_0.geojson"
-council_id = 'E07000111'
+council_id = 'SEV'
 
 stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
 stations_scraper.scrape()
