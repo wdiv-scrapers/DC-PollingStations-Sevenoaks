a0814836affb3a0b53eb1a47081f52acf8a43762
GeoWill <roper.william@gmail.com>
2024-04-16 17:24:35 +0100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ stations_url = "https://utility.arcgis.com/usrsvcs/servers/9c64a911bfee4ed9acd03
 districts_url = "https://utility.arcgis.com/usrsvcs/servers/ecffd85eb016496a92a0091064a77762/rest/services/OpenData/OD_PollingDistricts/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 council_id = 'SEV'
 
-stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
+stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations', key='OBJECTID')
 stations_scraper.scrape()
-districts_scraper = GeoJsonScraper(districts_url, council_id, 'utf-8', 'districts', key='OBJECTID')
+districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts', key='OBJECTID')
 districts_scraper.scrape()
