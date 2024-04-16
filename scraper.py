181ecf800683c3bcbf14831547c1ab9624a46038
GeoWill <roper.william@gmail.com>
2024-04-16 17:29:36 +0100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from dc_base_scrapers.geojson_scraper import GeoJsonScraper
+from dc_base_scrapers.geojson_scraper import ArcGisScraper
 
 stations_url = "https://utility.arcgis.com/usrsvcs/servers/9c64a911bfee4ed9acd032f03dcc8f06/rest/services/OpenData/OD_PollingStations/FeatureServer/0/query?outFields=*&where=1%3D1&f=json"
 districts_url = "https://utility.arcgis.com/usrsvcs/servers/ecffd85eb016496a92a0091064a77762/rest/services/OpenData/OD_PollingDistricts/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
